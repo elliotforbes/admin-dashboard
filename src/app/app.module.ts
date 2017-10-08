94107cff3975956285532d1829006d2dc18c5882
= <emforce@live.com>
2017-10-08 19:22:59 +0100

    More commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './common/top-nav/top-nav.component';
+import { SideNavComponent } from './common/side-nav/side-nav.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
